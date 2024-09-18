51aa878a89acdeb6487601bc1895206b6c1ebfb9
Severin Isermann <isermann.severin@gmail.com>
2024-09-18 18:58:32 +0200

    Tryed to create Json Structure

---
+++ scripts/objects.js
@@ -0,0 +1,56 @@
+let myDishes = [ 
+    "burger": {
+        "name": "Hamburger",
+        "price": 1.50,
+        "description": "Standard Hamburger mit einem Hackfleischpatty zwischen zwei Scheiben Burgerbuns.",
+        "amount":0,
+        },
+        {
+            "name": "Cheeseburger",
+            "price": 1.80,
+            "description": "Standard Hamburger mit einer Scheibe jungem Gouda",
+            "amount":0,
+        },
+        {
+            "name": "Royalburger",
+            "price": 2.60,
+            "description": "Großer Hamburger mit zwei Scheiben Hackfleischpattys mit frischem Salat, Tomate, jungem Gouda und unserer hausgemachten Spezialsoße.",
+            "amount": 0,
+        },
+    "pizza": {
+            "name": "Margerita",
+            "price": 7.00,
+            "description": "Frisch gebackener Pizzateig mit Tomatensoße und Emmentalerkäse",
+            "amount":0,
+        },
+        {
+            "name": "Prosciutto",
+            "price": 9.00,
+            "description": "Wie eine Margerita, mit herrlichem Vorderschinken",
+            "amount":0,
+        },
+        {
+            "name": "Salami",
+            "price": 9.00,
+            "description": "Wie eine Margarita, mit italienischer Salami",
+            "amount":0,
+        },
+    "beilagen":{
+            "name": "Panierte Mozzarella Sticks (6 Stück)",
+            "price": 7.50,
+            "description": "Geschmolzener Mozzarella ummantelt  mit einer knusprig gebackenen Schicht teig und dazu süß-saure Soße.",
+            "amount":0,
+        },
+        {
+            "name": "Panierte Jalapenos(SCharf 6 Stück)",
+            "price": 7.50,
+            "description": "Mit Chillisoße.",
+            "amount":0,
+        },
+        {
+            "name": "Pommes frites",
+            "price": 3.50,
+            "description": "Kartoffelstreifen mit ketchup.",
+            "amount":0,
+        },    
+]
